dc6b139de8473c670a8d5f56c88965ca77d50de7
Johan Guse <johanguse@gmail.com>
2022-08-04 00:41:39 -0300

    improve api

--- wp-content/themes/cavalotrucado-headless-wordpress/api/api-caminhao.php
+++ wp-content/themes/cavalotrucado-headless-wordpress/api/api-caminhao.php
@@ -1,59 +1,68 @@
 <?php
-function get_truck_by_slug3($request) {
+function get_truck_by_slug($request) {
   $slug = sanitize_text_field($request->get_param('slug'));
   $page_object = get_page_by_path($slug, OBJECT, 'vehicle');
-
   $id = $page_object->ID;
+  $brand = wp_get_object_terms($id, 'brand');
 
-  $vehicle = array(
-    'slug' => $slug,
+  $mainImage = get_field('vehicle_main_photo', $id);
+  if( $mainImage ):
+
+    // Image variables.
+    $url = $mainImage['url'];
+    $title = $mainImage['title'];
+    $alt = $mainImage['alt'];
+    $caption = $mainImage['caption'];
+
+    // Thumbnail size attributes.
+    $size = 'large';
+    $thumb = $mainImage['sizes'][ $size ];
+    $width = $mainImage['sizes'][ $size . '-width' ];
+    $height = $mainImage['sizes'][ $size . '-height' ];
+
+    $mainImageUrl = esc_url($thumb);
+
+  endif;
+
+  $vehicle['data'] = array(
     'id' => $id,
     'title' => $page_object->post_title,
+    'slug' => $slug,
+    'brand' => $brand[0]->name,
+    'brand_slug' => $brand[0]->slug,
     'vehicle_model_name' => get_field('vehicle_model_name', $id),
+    'vehicle_year' => get_field('vehicle_year', $id),
+    'vehicle_year_model' => get_field('vehicle_year_model', $id),
+    'vehicle_show_price' => get_field('vehicle_show_price', $id),
+    'vehicle_price' => get_field('vehicle_price', $id),
+    'vehicle_km' => get_field('vehicle_km', $id),
+    'vehicle_state' => get_field('vehicle_state', $id),
+    'vehicle_main_photo' => $mainImageUrl,
     'vehicle_long_text' => get_field('vehicle_long_text', $id),
+    'vehicle_short_text_1' => get_field('vehicle_short_text_1', $id),
+    'vehicle_short_text_2' => get_field('vehicle_short_text_2', $id),
+    'vehicle_short_text_3' => get_field('vehicle_short_text_3', $id),
   );
 
   $images = get_field('vehicle_photos', $id);
+  $i=0;
   foreach ($images as $image) {
-    $vehicle['photos'][] = array(
+    $vehicle['data']['photos'][$i] = array(
       'url' => $image['url'],
+      'alt' => $image['alt'],
+      'title' => $image['title'],
+      'caption' => $image['caption'],
+      'description' => $image['description'],
+      'sizes' => $image['sizes'],
+      'width' => $image['width'],
+      'height' => $image['height'],
     );
+    $i++;
   }
-
-
-
-/*
-  //Image Gallery
-    $images = get_field('vehicle_photos', $id);
-    if($images) {
-      $g = 0;
-      $data[]['vehicle_photos'] = [];
-        foreach($images as $image) {
-          $data['vehicle_photos'][$g] = $image['url'];
-
-          $g++;
-        }
-    }
-
-  //Image Gallery
-  
-  $images = get_field('vehicle_photos', $id);
-  if($images) {
-    $i = 0;
-    $gallery['vehicle_photos'] = [];
-      foreach($images as $i => $image) {
-        $gallery[$i] = $image['url'];
-        
-        $i++;
-      }
-
-  }*/
-
-  //$final = array_push($vehicle, $data);
 
   return rest_ensure_response($vehicle);
 }
-function get_truck_by_slug($request) {
+function get_truck_by_slug2($request) {
   $slug = sanitize_text_field($request->get_param('slug'));
 
   $args = [
@@ -106,22 +115,6 @@ function get_truck_by_slug($request) {
 		$i++;
 	}
 
-
-  /*
-  $images = get_field('vehicle_photos', $posts->ID);
-
-  if( $images ){
-  $g = 0;
-  $gallery = [];
-  $gallery[$g]['url'] = 'teste';
-    foreach($images as $image) {
-      $gallery[$g]['url'] = $image['url'];
-      $data[$i]['vehicle_model_name'] = get_field('vehicle_model_name', $post->ID);
-
-      $g++;
-    }
-  }*/
-
   $response = new WP_REST_Response($data);
   $response->set_status(200);
 
@@ -133,7 +126,7 @@ function get_random_4_trucks() {
   
 	$args = [
     'post_status' => 'publish',
-		'numberposts' => 4,
+		'numberposts' => 20,
     'orderby' => 'rand',
 		'post_type' => 'vehicle'
 	];
@@ -148,6 +141,25 @@ function get_random_4_trucks() {
 	$i = 0;
 
 	foreach($posts as $post) {
+    $mainImage = get_field('vehicle_main_photo', $post->ID);
+    if( $mainImage ):
+
+      // Image variables.
+      $url = $mainImage['url'];
+      $title = $mainImage['title'];
+      $alt = $mainImage['alt'];
+      $caption = $mainImage['caption'];
+
+      // Thumbnail size attributes.
+      $size = 'medium';
+      $thumb = $mainImage['sizes'][ $size ];
+      $width = $mainImage['sizes'][ $size . '-width' ];
+      $height = $mainImage['sizes'][ $size . '-height' ];
+
+      $mainImageUrl = esc_url($thumb);
+
+    endif;
+
     $brand = wp_get_object_terms($post->ID, 'brand');
 		$data[$i]['id'] = $post->ID;
 		$data[$i]['title'] = $post->post_title;
@@ -160,7 +172,7 @@ function get_random_4_trucks() {
     $data[$i]['vehicle_price'] = get_field('vehicle_price', $post->ID);
     $data[$i]['vehicle_km'] = get_field('vehicle_km', $post->ID);
     $data[$i]['vehicle_state'] = get_field('vehicle_state', $post->ID);
-    $data[$i]['vehicle_main_photo'] = get_field('vehicle_main_photo', $post->ID);
+    $data[$i]['vehicle_main_photo'] = $mainImageUrl;
     $data[$i]['vehicle_short_text_1'] = get_field('vehicle_short_text_1', $post->ID);
     $data[$i]['vehicle_short_text_2'] = get_field('vehicle_short_text_2', $post->ID);
     $data[$i]['vehicle_short_text_3'] = get_field('vehicle_short_text_3', $post->ID);
@@ -193,14 +205,12 @@ add_action('rest_api_init', function() {
 
 /**
  * Add cache to custom/v1/* endpoint.
- * Add ?skip_cache=1 to the end of url to skip cache.
+ * to skip cache add ?skip_cache=1 to the end of url.
  */
-
 function wprc_add_acf_posts_endpoint( $allowed_endpoints ) {
     if ( ! isset( $allowed_endpoints[ 'custom/v1' ] ) || ! in_array( 'random_4_trucks', $allowed_endpoints[ 'custom/v1' ] ) ) {
         $allowed_endpoints[ 'custom/v1' ][] = 'random_4_trucks';
-    }
-    if ( ! isset( $allowed_endpoints[ 'custom/v1' ] ) || ! in_array( 'truck', $allowed_endpoints[ 'custom/v1' ] ) ) {
+        $allowed_endpoints[ 'custom/v1' ][] = 'truck/(?P<slug>[a-zA-Z0-9-]+)';
         $allowed_endpoints[ 'custom/v1' ][] = 'truck';
     }
     return $allowed_endpoints;
