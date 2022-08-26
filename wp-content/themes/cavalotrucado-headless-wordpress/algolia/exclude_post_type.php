fbcb718e50ab09eefcb776512d1bcdbaf8642ab5
Johan Guse <johanguse@gmail.com>
2022-08-25 22:24:49 -0300

    improve algolia search index

--- wp-content/themes/cavalotrucado-headless-wordpress/algolia/exclude_post_type.php
+++ wp-content/themes/cavalotrucado-headless-wordpress/algolia/exclude_post_type.php
@@ -11,3 +11,24 @@ function wds_algolia_exclude_post_types($post_types)
   return $post_types;
 }
 add_filter('algolia_searchable_post_types', 'wds_algolia_exclude_post_types');
+
+/**
+ * @param bool    $should_index
+ * @param WP_Post $post
+ *
+ * @return bool
+ */
+function exclude_post_types($should_index, WP_Post $post)
+
+{
+  // Add all post types you don't want to make searchable.
+  $excluded_post_types = array('page', 'post');
+  if (false === $should_index) {
+    return false;
+  }
+
+  return !in_array($post->post_type, $excluded_post_types, true);
+}
+
+// Hook into Algolia to manipulate the post that should be indexed.
+add_filter('algolia_should_index_searchable_post', 'exclude_post_types', 10, 2);
--- wp-content/themes/cavalotrucado-headless-wordpress/algolia/push_acf.php
+++ wp-content/themes/cavalotrucado-headless-wordpress/algolia/push_acf.php
@@ -1,5 +1,14 @@
 <?php
 
+
+function convert_chars_to_entities($str)
+{
+  $str = str_replace('&#215;', '', $str); // Yeah, I know.  But otherwise the gap is confusing.  --Kris
+
+
+  return $str;
+}
+
 add_filter('algolia_post_shared_attributes', 'vehicle_attributes', 10, 2);
 add_filter('algolia_searchable_post_shared_attributes', 'vehicle_attributes', 10, 2);
 
@@ -20,9 +29,16 @@ function vehicle_attributes(array $attributes, WP_Post $post)
 
   // Get the field value with the 'get_field' method and assign it to the attributes array.
   // @see https://www.advancedcustomfields.com/resources/get_field/
+  $slug = basename(get_permalink($post->ID));
+  $attributes['slug'] = $slug;
   $attributes['vehicle_model_name'] = get_field('vehicle_model_name', $post->ID);
   $attributes['vehicle_year'] = get_field('vehicle_year', $post->ID);
   $attributes['vehicle_year_model'] = get_field('vehicle_year_model', $post->ID);
+  //Long Text
+  $long_text = get_field('vehicle_long_text', $post->ID);
+  $long_text_no_html = strip_tags($long_text);
+  $long_text_no_html = convert_chars_to_entities($long_text_no_html);
+  $attributes['vehicle_long_text'] = $long_text_no_html;
   //Brand
   $brand = wp_get_object_terms($post->ID, 'brand');
   $attributes['brand'] = $brand[0]->name;
